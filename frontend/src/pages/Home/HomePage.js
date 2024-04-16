61e48d7969eedec67273fb6a7b83df1cc9aac37f
ChrisLu1109 <111708723+ChrisLu1109@users.noreply.github.com>
2024-04-15 21:27:26 -0500

    1

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useReducer } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { filterFoods, getAllTags, search as searchFoods } from "../../services/foodService";
+import { Link, useLocation, useParams } from "react-router-dom";
+import {
+  filterFoods,
+  getAllTags,
+  search as searchFoods,
+} from "../../services/foodService";
 import Thumbnails from "../../components/Thumbnails/Thumbnails";
 import SearchBar from "../../components/Search/Search";
 import Tags from "../../components/Tags/Tags";
@@ -8,8 +12,8 @@ import Tags from "../../components/Tags/Tags";
 const initialState = {
   foods: [],
   tags: [],
-  selectedTag: 'All',
-  searchTerm: ''
+  selectedTag: "All",
+  searchTerm: "",
 };
 
 const reducer = (state, action) => {
@@ -31,15 +35,26 @@ function HomePage() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { foods, tags, selectedTag, searchTerm } = state;
   const location = useLocation();
+  const { searchTerm: urlSearchTerm } = useParams();
 
   // Fetch all tags on component mount
   useEffect(() => {
-    getAllTags().then(tags => {
-      dispatch({ type: "LOAD_TAGS", payload: tags.map(tag => ({ name: tag })) });
+    getAllTags().then((tags) => {
+      dispatch({
+        type: "LOAD_TAGS",
+        payload: tags.map((tag) => ({ name: tag })),
+      });
     });
   }, []);
 
   // Fetch and load foods based on dietary restrictions or search terms
+
+  useEffect(() => {
+    if (urlSearchTerm) {
+      dispatch({ type: "SET_SEARCH_TERM", payload: urlSearchTerm });
+    }
+  }, [urlSearchTerm]);
+
   useEffect(() => {
     const restrictions = location.state?.selectedRestrictions || [];
     const fetchFoods = async () => {
@@ -55,29 +70,41 @@ function HomePage() {
     fetchFoods();
   }, [location.state, searchTerm]);
 
-  const handleTagClick = tagName => {
+  const handleTagClick = (tagName) => {
     dispatch({ type: "SET_TAG", payload: tagName });
   };
 
-  const handleSearch = term => {
+  const handleSearch = (term) => {
     dispatch({ type: "SET_SEARCH_TERM", payload: term });
   };
 
   // Get categorized foods based on the selected tag
-  const categorizedFoods = selectedTag === 'All' ? foods : foods.filter(food => food.tags?.includes(selectedTag));
+  const categorizedFoods =
+    selectedTag === "All"
+      ? foods
+      : foods.filter((food) => food.tags?.includes(selectedTag));
 
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "space-between", padding: "10px 20px" }}>
-        <Link to="/admin/setup" style={{
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
           padding: "10px 20px",
-          fontSize: "1.2em",
-          background: "#007bff",
-          color: "white",
-          textDecoration: "none",
-          borderRadius: "5px",
-          boxShadow: "0 2px 4px rgba(0,123,255,0.3)",
-        }}>
+        }}
+      >
+        <Link
+          to="/admin/setup"
+          style={{
+            padding: "10px 20px",
+            fontSize: "1.2em",
+            background: "#007bff",
+            color: "white",
+            textDecoration: "none",
+            borderRadius: "5px",
+            boxShadow: "0 2px 4px rgba(0,123,255,0.3)",
+          }}
+        >
           Administrator Login
         </Link>
       </div>
